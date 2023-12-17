5348979ea37082211e998a47c96986a61e26b140
zazaka71 <zazaka71@gmail.com>
2023-12-17 21:33:26 +0600

    add like

--- app_like/models.py
+++ app_like/models.py
@@ -1,54 +1,24 @@
-from datetime import datetime
+from django.conf import settings
+from django.db import models
 
-from rest_framework import status
-from rest_framework.generics import GenericAPIView, get_object_or_404
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-from app_like.models import Like
-from app_like.serialisers import LikeSerializer
 from app_product.models import Product
 
 
-class LikeView(GenericAPIView):
-    """
-        create a object for like product by user
-    """
-
-    serializer_class = LikeSerializer
-    permission_classes = (
-        IsAuthenticated,
+class Like(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='likes',
     )
-
-    def post(self, request):
-        srz_data = self.serializer_class(data=request.data)
-        if srz_data.is_valid(raise_exception=True):
-            product_id = srz_data.validated_data['product_id']
-            try:
-                Like.objects.get(product__id=product_id, user=request.user)
-                return Response({'message': 'this product already liked by you'}, status=status.HTTP_400_BAD_REQUEST)
-            except Like.DoesNotExist:
-                product = get_object_or_404(Product.confirmed, pk=product_id)
-                Like.objects.create(user=request.user, product=product, register_date=datetime.now())
-                return Response({'message': 'liked success'}, status=status.HTTP_200_OK)
-
-
-class DisLikeView(GenericAPIView):
-    """
-        create a object for like product by user
-    """
-
-    serializer_class = LikeSerializer
-    permission_classes = (
-        IsAuthenticated,
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        related_name='likes',
     )
+    register_date = models.DateTimeField()
 
-    def post(self, request):
-        srz_data = self.serializer_class(data=request.data)
-        if srz_data.is_valid(raise_exception=True):
-            product_id = srz_data.validated_data['product_id']
-            try:
-                Like.objects.get(product__id=product_id, user=request.user).delete()
-                return Response({'message': 'like for this produc deleted success'}, status=status.HTTP_200_OK)
-            except Like.DoesNotExist:
-                return Response({'message': 'you not like this product'}, status=status.HTTP_400_BAD_REQUEST)
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return str(self.register_date)
